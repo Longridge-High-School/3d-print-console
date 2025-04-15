d85acdf585ed2663c6ef2715659d7cf5098be30f
Alex Paterson <agp@lhs.lancs.sch.uk>
2025-04-15 15:12:50 +0100

    Fixed bug causing logs to be unviewable from SFTP.

--- default/copy.sh
+++ default/copy.sh
@@ -3,14 +3,17 @@
 if [ ! -f  "/data/printers.json" ];
 then
     cp /defaults/printers.json /data/printers.json
+    chmod +776 /data/printers.json
 fi
 
 if [ ! -f  "/data/cameras.json" ];
 then
     cp /defaults/cameras.json /data/cameras.json
+    chmod +776 /data/cameras.json
 fi
 
 if [ ! -f  "/data/config.json" ];
 then
     cp /defaults/config.json /data/config.json
+    chmod +776 /data/config.json
 fi
--- logs/index.php
+++ logs/index.php
@@ -6,20 +6,20 @@
         $ip = htmlspecialchars ($_SERVER ["HTTP_X_FORWARDED_FOR"]); // Work around proxy. Ideally, this should be $_SERVER ["REMOTE_ADDR"].
         $entry = htmlspecialchars ($_POST ["entry"]);
         
-        $file = fopen ("/log/log.txt", "a");
+        $file = fopen ("/data/log.txt", "a");
         fwrite ($file, $time . " - " . $ip . ' - ' . $entry . "\n");
         fclose ($file);
 
-        if (filesize ("/log/log.txt") > (1024 * 2048)) // Maximum file size 2 Mb.
+        if (filesize ("/data/log.txt") > (1024 * 2048)) // Maximum file size 2 Mb.
         {
-            $contents = explode ("\n", file_get_contents ("/log/log.txt"));
+            $contents = explode ("\n", file_get_contents ("/data/log.txt"));
 
             for ($i = 0; $i < 100; $i++)
             {
                 array_shift ($contents); // Delete first 100 entries.
             }
             
-            file_put_contents ("/log/log.txt", implode (PHP_EOL, $contents));
+            file_put_contents ("/data/log.txt", implode (PHP_EOL, $contents));
         }
     }
 ?>
